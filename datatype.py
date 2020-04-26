efa5fa2ea6c144f41090a1695e60449344eb02b9
keenmukund <mukundzplace@gmail.com>
2020-04-26 23:54:12 +0530

    DataTypes

---
+++ datatype.py
@@ -0,0 +1,15 @@
+a=10
+print(type(a))
+b=input()
+print(type(b))
+print(id(a))
+print(bin(a))
+c=True
+print(type(c))
+print(True+True)
+print(True+False)
+s='''data
+       base'''
+print(s)
+s='hello, "python"'
+print(s)
