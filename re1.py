d98c17d6d2f8bfe6c8113a134fafef233b7683fb
mukund-23 <mukundzplace@gmail.com>
2020-05-18 23:39:04 +0530

    re updated

--- re1.py
+++ re1.py
@@ -3,3 +3,14 @@ import re
 xx = "guru99,education is fun"
 r1 = re.findall(r"^\w+",xx)
 print (r1)
+
+
+phone = "2004-959-559 # This is Phone Number"
+
+
+num = re.sub(r'#.*$', "", phone)
+print ("Phone Num : ", num)
+
+
+num = re.sub(r'\D', "", phone)    
+print ("Phone Num : ", num)
